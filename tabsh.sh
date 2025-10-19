06de813221e8711fcc1f2ec15a1c1eb83d244882
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2025-10-19 18:34:40 +0400

    SHELL Enviorment Variable autochange

--- tabsh.sh
+++ tabsh.sh
@@ -2,8 +2,21 @@
 
 set -e # ends if error
 
+prev_shell=$SHELL # Record shell before change
+
+if [    $(pwd) == "/bin/tabsh"  ]; then # check if the shell is in /bin/
+    echo
+else
+    echo "WARNING: Shell not in /bin" # Warn if not
+    sleep 1
+fi
+
+
+export SHELL="$(pwd)/tabsh.sh" # change shell
 if [    "$1" == ""  ]; then # if there is no arguments run the shell, otherwise run the script given
     python3 tabsh.py 
 else
     python3 tabsh.py $1
 fi
+
+export SHELL="$prev_shell" # change shell after exit back to normal
